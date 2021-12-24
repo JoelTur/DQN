a0a60c8743bf59a5fd47d59a5f9c18fe9a9c5a55
Neuroosi <Neuroosi@users.noreply.github.com>
2021-12-24 16:59:26 +0200

    Update

--- DQN_FLAPPY.py
+++ DQN_FLAPPY.py
@@ -1,15 +1,15 @@
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.optimizers import Adam
+from keras.optimizers import gradient_descent_v2
 import random
 import numpy as np
 from collections import deque
 from ple.games.flappybird import FlappyBird
 from ple import PLE
-learning_rate = 0.001
+learning_rate = 0.00001
 REPLAY_MEMORY_SIZE=2000
 BATCH_SIZE = 32
-GAMMA = 0.95
+GAMMA = 0.99
 EPISODES = 5000
 class DQN:
     def __init__(self, actionSpaceSize, obsSpaceSize):
@@ -22,46 +22,46 @@ class DQN:
         self.epsilon = 1.0
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.99
-        self.update = 0
     def create_model(self):
         model = Sequential()
-        model.add(Dense(128,  input_shape = (self.obsSpaceSize,),activation ='relu' ))
-        model.add(Dense(128, activation = 'relu'))
+        model.add(Dense(64,  input_shape = (self.obsSpaceSize,),activation ='relu' ))
+        model.add(Dense(64, activation = 'relu'))
         model.add(Dense(self.actionSpaceSize, activation = 'linear'))
-        model.compile(loss = 'mse', optimizer=Adam(lr=learning_rate))
+        model.compile(loss = 'mse', optimizer=gradient_descent_v2.SGD(lr=learning_rate))
         return model
 
     def update_replay_memory(self, transition):
         self.replay_memory.append(transition)
 
-    def train(self, t_state):
+    def train(self,t):
         batch = random.sample(self.replay_memory, BATCH_SIZE)
+        states = [transition[0] for transition in batch]
+        next_states = [transition[3] for transition in batch]
         X = []
         Y = []
-        for state, action, reward, next_state, done in batch:
-            y = self.model.predict(np.array([state]))
+        y = self.model.predict(np.array(states))
+        target_y = self.target_model.predict(np.array(next_states))
+        for i,(state, action, reward, next_state, done) in enumerate(batch):
             if done:
-                y[0][action] = reward
+                y[i][action] = reward
             else:
-                a = self.target_model.predict(np.array([next_state]))[0]
-                y[0][action] = reward + GAMMA*np.amax(a)
+                y[i][action] = reward + GAMMA*np.amax(target_y[i])
             X.append(state)
-            Y.append(y)
-        self.model.fit(np.array(X), np.squeeze(np.array(Y)), epochs = 1, verbose = 0)
-            #self.model.fit(np.array([state]), np.array(y), epochs = 1, verbose = 0)
+            Y.append(y[i])
+        print(np.array(Y))
+        self.model.train_on_batch(np.array(X), np.array(Y))
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
-        if t_state == True:
-            self.update += 1
-        if self.update > 10:
+        if t  % 20 == 0:
             self.target_model.set_weights(self.model.get_weights())
-            self.update = 0
 
     def getPrediction(self, state):
         if np.random.rand() > self.epsilon:
             return np.argmax(self.model.predict(np.array([state])))
-        ##print("RANDOM")
         return random.randrange(self.actionSpaceSize)
+
+    def saveModel(self):
+        self.model.save("/home/joel/Flappybird-DQN/bestmodel")
 
 def processGameState(state):
     processed = np.array([])
@@ -95,14 +95,15 @@ if __name__ == "__main__":
             action = agent.getPrediction(state)
             reward = p.act(p.getActionSet()[action])
             if p.game_over() == True:
-                reward = -1000
+                reward = -1
             next_state = processGameState(p.getGameState())
             agent.update_replay_memory((state, action, reward, next_state, p.game_over()))
             state = next_state
             if len(agent.replay_memory) > BATCH_SIZE:
-                agent.train(p.game_over())
+                agent.train(t)
             t+=1
             if p.game_over() == True:
+                agent.saveModel()
                 break
         print("Time:", t, " Episode:", episode, " Epsilon:", agent.epsilon)
     

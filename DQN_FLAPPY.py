7db4941bd433e837dcb13d8daa6899abd4dfd3cd
Neuroosi <Neuroosi@users.noreply.github.com>
2021-12-25 13:25:04 +0200

    Update

--- DQN_FLAPPY.py
+++ DQN_FLAPPY.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import adam_v2
+from tensorflow import keras
 import random
 import numpy as np
 from collections import deque
@@ -8,12 +9,12 @@ from ple.games.flappybird import FlappyBird
 from ple import PLE
 
 ##HYPERPARAMETERS
-learning_rate = 0.0001
+learning_rate = 0.001
 REPLAY_MEMORY_SIZE=100000
 BATCH_SIZE = 64
 GAMMA = 0.99
 EPISODES = 5000
-EPSILON = 1.0
+EPSILON = 1
 EPSILON_MIN = 0.1
 EPSILON_DECAY = (EPSILON-EPSILON_MIN)/30000
 
@@ -28,8 +29,8 @@ class DQN:
 
     def create_model(self):
         model = Sequential()
-        model.add(Dense(256,input_shape = (self.obsSpaceSize,) ))
-        model.add(Dense(256, activation = 'relu'))
+        model.add(Dense(64,input_shape = (self.obsSpaceSize,) ))
+        model.add(Dense(64, activation = 'relu'))
         ##model.add(Dense(64, activation = 'relu'))
         model.add(Dense(self.actionSpaceSize))
         model.compile(loss = 'mse', optimizer=adam_v2.Adam(lr=learning_rate), metrics = ['accuracy'])
@@ -65,13 +66,23 @@ class DQN:
 
     def saveModel(self):
         self.model.save("/home/joel/Flappybird-DQN/bestmodel")
+    def loadModel(self):
+        self.model = keras.models.load_model("/home/joel/Flappybird-DQN/bestmodel/")
 
 if __name__ == "__main__":
     game = FlappyBird(288,512,100)
-    p = PLE(game, force_fps = True, frame_skip=3, display_screen=False)
+    answer = input("Display screen y/n? ")
+    display_screen = False
+    if answer is "y":
+        display_screen = True
+    p = PLE(game, force_fps = True, frame_skip=3, display_screen=display_screen)
     p.init()
     state = np.array(list(p.getGameState().values()))
     agent = DQN(len(p.getActionSet()), state.shape[0])
+    answer = input("Use a pre-trained model y/n? ")
+    if answer is "Y":
+        agent.loadModel()
+        EPSILON = 0.1
     t = 0
     rewards = []
     episodes = []
@@ -84,7 +95,7 @@ if __name__ == "__main__":
             action = agent.getPrediction(state)
             reward = p.act(p.getActionSet()[action])
             if p.game_over():
-                reward = -1000
+                reward = -1
             next_state = np.array(list(p.getGameState().values()))
             agent.update_replay_memory((state, action, reward, next_state, p.game_over()))
             state = next_state
